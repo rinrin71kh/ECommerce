619d5ead533419a8b129c14aa28add315646d32a
Korb Daven <korbdaven@gmail.com>
2024-10-23 08:24:39 +0700

    first code

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    This is my first VueJs project!
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
