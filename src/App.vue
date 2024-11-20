f705a52774177641c53b5293772aaa8f5eda833f
khuoy borin satya <khuoyborin@gmail.com>
2024-11-20 21:28:24 +0700

    seed data

--- src/App.vue
+++ src/App.vue
@@ -3,75 +3,71 @@ import axios from 'axios';
 import ButtonComponent from './components/ButtonComponent.vue';
 import CategoryComponent from './components/CategoryComponent.vue';
 import PromotionComponent from './components/PromotionComponent.vue';
-export default{
+
+export default {
   name: 'App',
-  components:{
+  components: {
     ButtonComponent,
     CategoryComponent,
-    PromotionComponent
+    PromotionComponent,
   },
   data() {
     return {
       Categories: [],
       Promotions: [],
-    
-    }
-},
-  methods:{
-    fetchCategories() { 
-      axios.get('http://localhost:3000/api/categories')
-      .then(response => {
-        // Access the data
-        // console.log(response.data); 
-        this.Categories = response.data;
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-  });
-         },
-    fetchPromotions() {
-      axios.get('http://localhost:3000/api/promotions')
-      .then(response => {
-        // Access the data
-        // console.log(response.data); 
-        this.Promotions = response.data;
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-  });
-          
-         }
+    };
   },
-
-  mounted () {
-          // Mounted life cycle - It will be executed every time
-          // this component is loaded
-
-          
-          this.fetchCategories()
-          this.fetchPromotions()
-     }
-}
+  methods: {
+    fetchCategories() {
+      axios
+        .get('http://localhost:3000/api/categories')
+        .then(response => {
+          console.log('Categories:', response.data);
+          this.Categories = response.data;
+        })
+        .catch(error => {
+          console.error('Error fetching categories:', error);
+        });
+    },
+    fetchPromotions() {
+      axios
+        .get('http://localhost:3000/api/promotions')
+        .then(response => {
+          console.log('Promotions:', response.data);
+          this.Promotions = response.data;
+        })
+        .catch(error => {
+          console.error('Error fetching promotions:', error);
+        });
+    },
+  },
+  mounted() {
+    this.fetchCategories();
+    this.fetchPromotions();
+  },
+};
 </script>
 
 <template>
+  <div>
+    <div class="category" v-if="Categories.length">
+      <template v-for="category in Categories" :key="category.id">
+        <CategoryComponent :name="category.name" :amount="category.productCount" :color="category.color" :image="category.image" />
+      </template>
+    </div>
+    <div v-else>No categories available</div>
 
-  <div class="category">
-    <template v-for="category in Categories">
-      <CategoryComponent :name="category.name" :amount="category.productCount" :color="category.color" :image="category.image"></CategoryComponent>
-    </template>
+    <div class="promotion" v-if="Promotions.length">
+      <template v-for="promotion in Promotions" :key="promotion.id">
+        <PromotionComponent :title="promotion.title" :color="promotion.color" :btnColor="promotion.buttonColor" :image="promotion.image" />
+      </template>
+    </div>
+    <div v-else>No promotions available</div>
   </div>
-  <div class="promotion">
-    <template v-for="promotion in Promotions">
-      <PromotionComponent :title="promotion.title" :color="promotion.color" :btnColor="promotion.buttonColor" :image="promotion.image"></PromotionComponent>
-    </template>
-  </div>
-  
-  
 </template>
 
 <style scoped>
-.category{
+.category {
   padding-top: 20px;
   margin: auto;
   width: 90%;
@@ -79,9 +75,8 @@ export default{
   justify-content: space-evenly;
   align-items: center;
   gap: 10px;
-  /* overflow-x: auto; */
 }
-.promotion{
+.promotion {
   padding-top: 40px;
   margin: auto;
   width: 90%;
