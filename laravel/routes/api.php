8a740d48692a18e185fd809e359bc74472cc77bb
khuoy borin satya <khuoyborin@gmail.com>
2025-03-05 22:21:21 +0700

    migrat data and define route

---
+++ laravel/routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+Route:: controller(CategoryController::class)->prefix('categories')->group(function(){
+    Route::get('/','getCategories');
+    Route::post('/','createCategories');
+    Route::get('/{categoryId}','getCategory');
+    Route::patch('/{categoryId}','updateCategory');
+    Route::delete('/{categoryId}','deleteCategory');
+});
+Route:: controller(ProductController::class)->prefix('products')->group(function(){
+    Route::get('/','getProducts');
+    Route::post('/','createProducts');
+    Route::get('/{productId}','getproduct');
+    Route::patch('/{productId}','updateProduct');
+    Route::delete('/{productId}','deleteProduct');
+});
