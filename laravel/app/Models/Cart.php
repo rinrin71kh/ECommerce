f5a22175089503625dc327371abdb63432629c7b
khuoy borin satya <khuoyborin@gmail.com>
2025-04-03 07:36:13 +0700

    start tp8

--- laravel/app/Models/Cart.php
+++ laravel/app/Models/Cart.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
+use Illuminate\Database\Eloquent\Relations\HasMany;
 class Cart extends Model
 {
     //
--- laravel/app/Models/Order.php
+++ laravel/app/Models/Order.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Carbon\Carbon;
-
+use Illuminate\Database\Eloquent\Relations\HasMany;
 class Order extends Model
 {
     //
