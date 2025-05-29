b571b0fc0b950f4e7ac0283ec75d38503e43f205
khuoy borin satya <khuoyborin@gmail.com>
2025-05-29 22:33:27 +0700

    finish tp10 backend

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
     })
   );
+
+  app.enableCors(); // <<< ADD THIS LINE
+
   await app.listen(process.env.PORT ?? 3100);
 }
 bootstrap();
